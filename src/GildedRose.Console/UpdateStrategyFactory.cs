5f3792d86c2ead295cc0f46f5c0994f4bf28dc79
Jace <jace.rhea@gmail.com>
2015-11-11 20:11:01 -0600

    Refactored

---
+++ src/GildedRose.Console/UpdateStrategyFactory.cs
@@ -0,0 +1,104 @@
+﻿namespace GildedRose.Console
+{
+    public class UpdateStrategyFactory : IUpdateStrategyFactory
+    {
+        public IUpdateStrategy Create(string name)
+        {
+            if (name == "Sulfuras, Hand of Ragnaros")
+            {
+                return new DoNothingUpdate();
+            }
+            else if (name == "Aged Brie")
+            {
+                return new AgedBrieUpdateStrategy();
+            }
+            else if (name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                return new BackstagePassesUpdateStrategy();
+            }
+            else if (name.Contains("Conjured"))
+            {
+                return new StandardUpdateStrategy(2);
+            }
+            else
+            {
+                return new StandardUpdateStrategy();
+            }
+        }
+    }
+
+    public class StandardUpdateStrategy : IUpdateStrategy
+    {
+        private readonly int _factor;
+
+        public StandardUpdateStrategy(int factor = 1)
+        {
+            _factor = factor;
+        }
+
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+            if (item.Quality > 0)
+            {
+                item.Quality -= _factor * (item.SellIn < 0 ? 2 : 1);
+            }
+            if (item.Quality < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+    }
+
+    public class BackstagePassesUpdateStrategy : IUpdateStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+            else if (item.SellIn <= 5)
+            {
+                item.Quality = item.Quality + 3;
+            }
+            else if (item.SellIn <= 10)
+            {
+                item.Quality = item.Quality + 2;
+            }
+            else if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
+        }
+    }
+
+    public class AgedBrieUpdateStrategy : IUpdateStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
+        }
+    }
+
+
+    public class DoNothingUpdate : IUpdateStrategy
+    {
+        public void UpdateQuality(Item item) { }
+    }
+
+    public interface IUpdateStrategyFactory
+    {
+        IUpdateStrategy Create(string name);
+    }
+
+    public interface IUpdateStrategy
+    {
+        void UpdateQuality(Item item);
+    }
+}
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using GildedRose.Console;
 using Xunit;
 
 namespace GildedRose.Tests
@@ -5,9 +8,66 @@ namespace GildedRose.Tests
     public class TestAssemblyTests
     {
         [Fact]
-        public void TestTheTruth()
+        public void TestUpdateOnZeroZeroItem()
         {
-            Assert.True(true);
+            var resultAndStrategyPair = new List<Tuple<Item, IUpdateStrategy>>
+            {
+                Tuple.Create(new Item {Quality = 0, SellIn = 0}, (IUpdateStrategy)new DoNothingUpdate()),
+                Tuple.Create(new Item {Quality = 1, SellIn = -1}, (IUpdateStrategy)new AgedBrieUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 0, SellIn = -1}, (IUpdateStrategy)new BackstagePassesUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 0, SellIn = -1}, (IUpdateStrategy)new StandardUpdateStrategy(2)),
+                Tuple.Create(new Item {Quality = 0, SellIn = -1}, (IUpdateStrategy)new StandardUpdateStrategy()),
+            };
+
+            foreach (var itemPair in resultAndStrategyPair)
+            {
+                var testCase = new Item { Quality = 0, SellIn = 0 };
+                itemPair.Item2.UpdateQuality(testCase);
+                Assert.Equal(itemPair.Item1.Quality, testCase.Quality);
+                Assert.Equal(itemPair.Item1.SellIn, testCase.SellIn);
+            }
+        }
+
+        [Fact]
+        public void TestUpdateOnTenTenItem()
+        {
+            var resultAndStrategyPair = new List<Tuple<Item, IUpdateStrategy>>
+            {
+                Tuple.Create(new Item {Quality = 10, SellIn = 10}, (IUpdateStrategy)new DoNothingUpdate()),
+                Tuple.Create(new Item {Quality = 11, SellIn = 9}, (IUpdateStrategy)new AgedBrieUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 12, SellIn = 9}, (IUpdateStrategy)new BackstagePassesUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 8, SellIn = 9}, (IUpdateStrategy)new StandardUpdateStrategy(2)),
+                Tuple.Create(new Item {Quality = 9, SellIn = 9}, (IUpdateStrategy)new StandardUpdateStrategy()),
+            };
+
+            foreach (var itemPair in resultAndStrategyPair)
+            {
+                var testCase = new Item { Quality = 10, SellIn = 10 };
+                itemPair.Item2.UpdateQuality(testCase);
+                Assert.Equal(itemPair.Item1.Quality, testCase.Quality);
+                Assert.Equal(itemPair.Item1.SellIn, testCase.SellIn);
+            }
+        }
+
+        [Fact]
+        public void TestUpdateOnProvidedValues()
+        {
+            var items = new List<Tuple<Item, Item, IUpdateStrategy>>
+            {
+                Tuple.Create(new Item {Quality = 19, SellIn = 9}, new Item {Name = "+5 Dexterity Vest", Quality = 20, SellIn = 10}, (IUpdateStrategy) new StandardUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 1, SellIn = 1}, new Item {Name = "Aged Brie", SellIn = 2, Quality = 0}, (IUpdateStrategy) new AgedBrieUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 6, SellIn = 4}, new Item {Name = "Elixir of the Mongoose", Quality = 7, SellIn = 5}, (IUpdateStrategy) new StandardUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 80, SellIn = 0}, new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80}, (IUpdateStrategy) new DoNothingUpdate()),
+                Tuple.Create(new Item {Quality = 21, SellIn = 14}, new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 20, SellIn = 15}, (IUpdateStrategy) new BackstagePassesUpdateStrategy()),
+                Tuple.Create(new Item {Quality = 4, SellIn = 2}, new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}, (IUpdateStrategy) new StandardUpdateStrategy(2))
+            };
+
+            foreach (var itemPair in items)
+            {
+                itemPair.Item3.UpdateQuality(itemPair.Item2);
+                Assert.Equal(itemPair.Item1.Quality, itemPair.Item2.Quality);
+                Assert.Equal(itemPair.Item1.SellIn, itemPair.Item2.SellIn);
+            }
         }
     }
 }
